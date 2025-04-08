cd5426ab463c9fc7a77cf0f6f18cd32c1a0b6473
Evan Walter Clark Spotte-Smith, PhD <espottesmith@gmail.com>
2025-04-07 20:11:53 -0400

    Everything works, but deployment...?

--- docs/make.jl
+++ docs/make.jl
@@ -18,6 +18,6 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/espottesmith/SimpleDirectedHypergraphs.jl",
+    repo="github.com/CoReACTER/SimpleDirectedHypergraphs.jl",
     devbranch="main",
 )
