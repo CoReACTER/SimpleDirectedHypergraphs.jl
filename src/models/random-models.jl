3a36e683d15404c26d35e3a803f8a965d523451b
Evan Walter Clark Spotte-Smith, PhD <espottesmith@gmail.com>
2026-08-05 13:06:42 +0100

    Random models done; I think that's all

--- src/models/random-models.jl
+++ src/models/random-models.jl
@@ -5,20 +5,39 @@
 
 
 """
-    random_model(nVertices::Int, nEdges::Int, HType::Type{H}) where {H<:AbstractDirectedHypergraph}
+    random_model(
+	nVertices::Int,
+	nEdges::Int,
+	HType::Type{H};
+	no_self_loops::Bool
+    ) where {H<:AbstractDirectedHypergraph}
 
-Generate a *random* directed hypergraph (in the style of Erdős–Rényi random graphs) without any structural constraints.
+Generate a *random* dihypergraph (in the style of Erdős–Rényi random graphs) without any structural
+constraints.
 
-# The Algorithm
+**Algorithm**
 
-Given two integer parameters *nVertices* and *nEdges* (the number of nodes and hyperedges, respectively),
-the algorithm computes - for each hyperedge *he={1,...,m}* -
-two random numbers *s_t ϵ [1, n]* (i.e., the size of the hyperedge tail) and *s_h ϵ [1, n]*
-(i.e., the size of the hyperedge head).
-Then, the algorithm selects uniformly at random *s_t* vertices from *V* to be added in the tail of *he*
-and *s_h* vertices from *V* to be added into the head of *he*.
-If *no_self_loops* is true (default false), then vertices will be chosen such that the same vertex *v*
-cannot appear in both the head and the tail of the same hyperedge *he*.
+Given two integer parameters `nVertices` and `nEdges` (the number of nodes and dihyperedges,
+respectively), the algorithm computes - for each hyperedge `e={1,...,m}` - two random numbers
+`s_t ϵ [1, n]` (i.e., the size of the tail) and `s_h ϵ [1, n]` (i.e., the size of the head).
+
+Then, the algorithm selects uniformly at random `s_t` vertices from among `[1,...,nVertices]` to be
+added in the tail of `e` and `s_h` vertices to be added into the head of `e`. If `no_self_loops` is
+`true` (default `false`), then vertices will be chosen such that the same vertex `v` cannot appear
+in both the head and the tail of the same hyperedge `e`.
+
+**Arguments**
+
+* `nVertices` : Number of vertices
+* `nEdges` : Number of dihyperedges
+* `HType` : Dihypergraph type (default `DirectedHypergraph`)
+* `no_self_loops`: If `true` (default `false`), then vertices in the tail of a dihyperedge cannot
+    also be in the head of that same dihyperedge
+
+**Returns**
+
+A random dihypergraph of type `HType` with `nVertices` vertices and `nEdges` dihyperedges
+
 """
 function SimpleHypergraphs.random_model(
         nVertices::Int,
@@ -28,7 +47,10 @@ function SimpleHypergraphs.random_model(
     ) where {H <: AbstractDirectedHypergraph}
     if no_self_loops && nVertices == 1
         # Impossible; all directed hyperedges with non-empty tails & heads will be self-loops
-        error("Impossible to avoid self-loops in non-empty directed hyperedges in a directed hypergraph with one (1) vertex!")
+        error(
+            "Impossible to avoid self-loops in non-empty directed hyperedges in a directed" *
+                "hypergraph with one (1) vertex!"
+        )
     end
 
     mx_tail = Matrix{Union{Nothing, Bool}}(nothing, nVertices, nEdges)
@@ -47,7 +69,12 @@ function SimpleHypergraphs.random_model(
         mx_tail[sample(1:size(mx_tail, 1), nv; replace = false), e] .= true
 
         if no_self_loops
-            valid_indices = collect(setdiff(Set(1:size(mx_tail, 1)), Set(findall(x -> x === true, mx_tail[:, e]))))
+            valid_indices = collect(
+                setdiff(
+                    Set(1:size(mx_tail, 1)),
+                    Set(findall(x -> x === true, mx_tail[:, e]))
+                )
+            )
             nv = rand(1:length(valid_indices))
             mx_head[sample(valid_indices, nv; replace = false), e] .= true
         else
@@ -67,14 +94,34 @@ end
 
 # TODO: overwrite issue
 """
-    random_kuniform_model(nVertices::Int, nEdges::Int, k::Int; HType::Type{H}=DirectedHypergraph, no_self_loops::Bool=false) where {H<:AbstractDirectedHypergraph}
+    random_kuniform_model(
+	nVertices::Int,
+	nEdges::Int,
+	k::Int,
+	HType::Type{H};
+	no_self_loops::Bool
+    ) where {H<:AbstractDirectedHypergraph}
 
-Generates a *k*-uniform directed hypergraph, i.e., an hypergraph where each hyperedge has size *k = k_tail + k_head*.
-In this implementation, *k_tail* and *k_head* are not necessarily equal.
+Generates a `k`-uniform dihypergraph, i.e., a dihypergraph where each dihyperedge has size
+`k = k_tail + k_head`. In this implementation, `k_tail` and `k_head` are not necessarily equal.
 
-# The Algorithm
+**The Algorithm**
 
-The algorithm proceeds as the *random_model*, forcing the size of each hyperedge equal to *k*.
+The algorithm proceeds as the `random_model`, forcing the size of each dihyperedge equal to `k`.
+
+**Arguments**
+
+* `nVertices` : Number of vertices
+* `nEdges` : Number of dihyperedges
+* `k` : Constant total dihyperedge size
+* `HType` : Dihypergraph type (default `DirectedHypergraph`)
+* `no_self_loops`: If `true` (default `false`), then vertices in the tail of a dihyperedge cannot
+    also be in the head of that same dihyperedge
+
+**Returns**
+
+A random `k`-uniform dihypergraph of type `HType` with `nVertices` vertices and `nEdges` dihyperedges
+
 """
 function SimpleHypergraphs.random_kuniform_model(
         nVertices::Int,
@@ -125,15 +172,29 @@ Generates a *d*-regular directed hypergraph, where each node has degree *d*.
 
 # The Algorithm
 
-The algorithm exploits the *k*-uniform approach described for the *random_kuniform_model* method
-to build a *d*-regular hypergraph *H* having *nVertices* nodes and *nEdges* edges.
-It returns the hypergraph *H^* dual of *H*.
+The algorithm exploits the `d`-uniform approach described for the `random_kuniform_model` method
+to build a `d`-regular hypergraph `h` having `nVertices` nodes and `nEdges` edges. It returns the
+hypergraph `h'` dual of `h`.
+
+**Arguments**
+
+* `nVertices` : Number of vertices
+* `nEdges` : Number of dihyperedges
+* `d` : Constant total dihyperedge size
+* `HType` : Dihypergraph type (default `DirectedHypergraph`)
+* `no_self_loops`: If `true` (default `false`), then vertices in the tail of a dihyperedge cannot
+    also be in the head of that same dihyperedge
+
+**Returns**
+
+A random `d`-regular dihypergraph of type `HType` with `nVertices` vertices and `nEdges` dihyperedges
+
 """
 function SimpleHypergraphs.random_dregular_model(
         nVertices::Int,
         nEdges::Int,
         d::Int,
-        HType::Type{H};
+        HType::Type{H} = DirectedHypergraph;
         no_self_loops::Bool = false
     ) where {H <: AbstractDirectedHypergraph}
     if no_self_loops && nVertices == 1
